70b6dfda9b1881257a6128a0b149b14a59cb384f
yassertawfek1990 <42835902+yassertawfek1990@users.noreply.github.com>
2024-09-11 22:55:51 +0000

    components function

--- src/intro.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-
-ReactDom.render(
-  <div>
-    <h1>Hello Yasser </h1> <p>Good Job</p>
-  </div>,
-  document.getElementById("root")
-);
